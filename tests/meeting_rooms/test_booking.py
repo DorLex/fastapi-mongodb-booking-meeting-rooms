0ecae5d45cd962f3f420968f2066243157e98757
DorLex <dorozhinskiysanya@gmail.com>
2024-06-24 16:17:48 +0300

    test(booking): update tests

--- tests/meeting_rooms/test_booking.py
+++ tests/meeting_rooms/test_booking.py
@@ -24,19 +24,68 @@ class TestBookingMeetingRooms:
     @pytest.mark.parametrize(
         'room_data',
         [
-            base_room_data,
+            base_room_data,  # дубликат слота бронирования комнаты
             {
                 'title': 'Главная',
                 'booking_start_at': '2024-06-17 11:00',
-                'booking_end_at': '2024-06-17 12:30'
+                'booking_end_at': '2024-06-17 12:30'  # пересечение времени окончания
             },
             {
                 'title': 'Главная',
-                'booking_start_at': '2024-06-17 13:00',
+                'booking_start_at': '2024-06-17 13:00',  # пересечение времени начала
                 'booking_end_at': '2024-06-17 15:00'
             }
         ]
     )
-    async def test_booking_occupied_room(self, client: AsyncClient, room_data):
+    async def test_booking_occupied_room(self, client: AsyncClient, room_data: dict):
         response = await client.post(self.url, json=room_data, headers=self.auth_headers)
         assert response.status_code == status.HTTP_400_BAD_REQUEST, response.text
+
+    async def test_booking_room_incorrect_name(self, client: AsyncClient):
+        room_data = {
+            'title': 'Неправильное название комнаты',
+            'booking_start_at': '2024-06-17 01:00',
+            'booking_end_at': '2024-06-17 02:00'
+        }
+
+        response = await client.post(self.url, json=room_data, headers=self.auth_headers)
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY, response.text
+
+    @pytest.mark.parametrize(
+        'room_data',
+        [
+            {
+                'title': 'Главная',
+                'booking_start_at': '01:00 18.06.2024',  # неверный шаблон datetime
+                'booking_end_at': '02:00 18.06.2024'
+            },
+            {
+                'title': 'Главная',
+                'booking_start_at': '2024-06-18 01:07',  # неверный шаблон минут
+                'booking_end_at': '2024-06-20 02:15'
+            },
+            {
+                'title': 'Главная',
+                'booking_start_at': '2024-06-17 01:00',  # одинаковое время начала и конца
+                'booking_end_at': '2024-06-17 01:00'
+            },
+            {
+                'title': 'Главная',
+                'booking_start_at': '2024-06-17 03:00',  # время начала бронирования > время конца
+                'booking_end_at': '2024-06-17 01:00'
+            },
+            {
+                'title': 'Главная',
+                'booking_start_at': '2024-06-18 01:00',  # бронирование больше 24 часов
+                'booking_end_at': '2024-06-19 01:30'
+            },
+            {
+                'title': 'Главная',
+                'booking_start_at': '2024-06-18 01:00',  # бронирование больше 24 часов
+                'booking_end_at': '2024-06-20 01:00'
+            },
+        ]
+    )
+    async def test_booking_room_incorrect_time(self, client: AsyncClient, room_data: dict):
+        response = await client.post(self.url, json=room_data, headers=self.auth_headers)
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY, response.text
